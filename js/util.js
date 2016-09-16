0a9ff2d03cc90dd13781bff7dc5ef1e0d699ef41
Chris Macrander <cmacrander@gmail.com>
2016-09-16 13:45:31 -0700

    adding Array.some(), Array.every()

--- js/util.js
+++ js/util.js
@@ -157,6 +157,86 @@
         };
     }
 
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+    if (typeof Array.prototype.every !== 'function') {
+        Array.prototype.every = function(callbackfn, thisArg) {
+            var T, k;
+            if (this == null) {
+                throw new TypeError('this is null or not defined');
+            }
+            // 1. Let O be the result of calling ToObject passing the this
+            //        value as the argument.
+            var O = Object(this);
+            // 2. Let lenValue be the result of calling the Get internal method
+            //        of O with the argument "length".
+            // 3. Let len be ToUint32(lenValue).
+            var len = O.length >>> 0;
+            // 4. If IsCallable(callbackfn) is false, throw a TypeError exception.
+            if (typeof callbackfn !== 'function') {
+                throw new TypeError();
+            }
+            // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+            if (arguments.length > 1) {
+                T = thisArg;
+            }
+            // 6. Let k be 0.
+            k = 0;
+            // 7. Repeat, while k < len
+            while (k < len) {
+                var kValue;
+                // a. Let Pk be ToString(k).
+                //     This is implicit for LHS operands of the in operator
+                // b. Let kPresent be the result of calling the HasProperty internal
+                //        method of O with argument Pk.
+                //     This step can be combined with c
+                // c. If kPresent is true, then
+                if (k in O) {
+                    // i. Let kValue be the result of calling the Get internal method
+                    //        of O with argument Pk.
+                    kValue = O[k];
+                    // ii. Let testResult be the result of calling the Call internal method
+                    //         of callbackfn with T as the this value and argument list
+                    //         containing kValue, k, and O.
+                    var testResult = callbackfn.call(T, kValue, k, O);
+                    // iii. If ToBoolean(testResult) is false, return false.
+                    if (!testResult) {
+                        return false;
+                    }
+                }
+                k++;
+            }
+            return true;
+        };
+    }
+
+    // Production steps of ECMA-262, Edition 5, 15.4.4.17
+    // Reference: http://es5.github.io/#x15.4.4.17
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+    if (!Array.prototype.some) {
+        Array.prototype.some = function(fun/*, thisArg*/) {
+
+            if (this == null) {
+                throw new TypeError('Array.prototype.some called on null or undefined');
+            }
+
+            if (typeof fun !== 'function') {
+                throw new TypeError();
+            }
+
+            var t = Object(this);
+            var len = t.length >>> 0;
+
+            var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+            for (var i = 0; i < len; i++) {
+                if (i in t && fun.call(thisArg, t[i], i, t)) {
+                    return true;
+                }
+            }
+
+            return false;
+        };
+    }
+
     // Polyfill of Object.create for IE8. The real thing is much fancier,
     // including "property descriptors", which are some kind of crazy alien
     // species with strange powers. But we don't need to use them.
