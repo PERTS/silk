deb4a81415c4fb2fe9e57361a3f335fe4aa93eff
Daniel Fiore <taptapdan@gmail.com>
2016-12-06 15:35:53 -0800

    Add Array.prototype.includes polyfill
    During code review of a Neptune pull request it was recommended by
    @cmacrander to update the Array.prototype.includes polyfill and
    also add an alias on .contains for legacy code.
    
    https://github.com/PERTS/neptune/pull/216

--- js/util.js
+++ js/util.js
@@ -329,10 +329,42 @@
 
 // Functions that aren't in the spec, but are useful in native prototypes.
 
-Array.prototype.contains = function (value) {
+// The includes() method determines whether an array includes a certain element,
+// returning true or false as appropriate.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+Array.prototype.includes = function(searchElement /*, fromIndex*/) {
     'use strict';
-    return this.indexOf(value) > -1;
+    if (this == null) {
+        throw new TypeError('Array.prototype.includes called on null or undefined');
+    }
+
+    var O = Object(this);
+    var len = parseInt(O.length, 10) || 0;
+    if (len === 0) {
+        return false;
+    }
+    var n = parseInt(arguments[1], 10) || 0;
+    var k;
+    if (n >= 0) {
+        k = n;
+    } else {
+        k = len + n;
+        if (k < 0) {k = 0;}
+    }
+    var currentElement;
+    while (k < len) {
+        currentElement = O[k];
+        if (searchElement === currentElement ||
+           (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+            return true;
+        }
+        k++;
+    }
+    return false;
 };
+
+// Alias for legacy code
+Array.prototype.contains = Array.prototype.includes;
 
 Array.prototype.last = function () {
     'use strict';
