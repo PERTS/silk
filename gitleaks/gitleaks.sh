68a964aec1f0f2719890620dd4bf3886afd23a92
Daniel Fiore <taptapdan@gmail.com>
2021-02-16 14:04:51 -0800

    Add some additional comments

--- gitleaks/gitleaks.sh
+++ gitleaks/gitleaks.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Run gitleaks https://github.com/zricethezav/gitleaks
+
+# Run gitleaks from any of our repositories with the following commands:
+#    /bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/PERTS/silk/HEAD/gitleaks/gitleaks.sh)\"
+
+# You probably also want to add the following to your .gitignore file:
+#    gitleaks*
+
+# More about gitleaks: https://github.com/zricethezav/gitleaks
 
 # Lines which have an error cause this whole script to exit with an error.
 # http://stackoverflow.com/questions/3474526/stop-on-first-error
@@ -20,13 +27,15 @@ if [ "$CI" = "true" ]; then
   export GITLEAKS_BINARY="https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_RELEASE}/gitleaks-linux-amd64"
 fi
 
+# Download the gitleaks binary if it hasn't already been downloaded.
 if ! [ -f ./${GITLEAKS_CMD} ]; then
   echo "[gitleaks] Downloading gitleaks binary."
   curl -o ${GITLEAKS_CMD} -L ${GITLEAKS_BINARY}
   chmod +x ${GITLEAKS_CMD}
 fi
 
-
+# To ensure we're always using the latest gitleaks rules/config file, redownload
+# the latest version from this repo.
 echo "[gitleaks] Downloading gitleaks config."
 curl -o gitleaks.toml -L ${GITLEAKS_CONFIG}
 
